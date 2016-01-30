be2c28417939e905c280a0ccefee3e7e0147d3d3
MachoMecha <fernandocavo@gmail.com>
2016-01-30 13:15:49 -0300

    Es Macho...

--- src/model/ORM/UsuarioRolEquipo.php
+++ src/model/ORM/UsuarioRolEquipo.php
@@ -7,4 +7,17 @@ class UsuarioRolEquipo extends EntityBase{
     protected $table = "UsuarioRolEquipo";
     
     protected $primaryKey = 'idUsuarioRolEquipo';
+
+		    public function idUsuario(){
+                return $this->belongsTo('Model\ORM\Usuario','idUsuario','idUsuario');
+            }
+
+            public function idRol(){
+                return $this->belongsTo('Model\ORM\Rol', 'idRol', 'idRol');
+            }
+
+            public function idEquipo(){
+                return $this->belongsTo('Model\ORM\Equipo','idEquipo','idEquipo');
+            }
+        
 }
