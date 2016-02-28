b373b0afd4be77a4eb280fc4576e6e07df3fdb75
MachoMecha <fernandocavo@gmail.com>
2016-02-28 17:07:28 -0300

    solucionando conflictos

--- web/js/application.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-$(document).ready(function(){alert("Entro a ready")});
-=======
-"use sctrict";!function(n){n.fn.myfunction=function(){}}(jQuery),$(document).ready(function(){$('button[rol="backButton"]').on("click",function(){window.history.go(-1)})});
->>>>>>> 080db5d8266f188a79ad865e63c5283d83756fed
