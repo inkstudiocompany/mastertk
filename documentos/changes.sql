b5662f78b17c01c0c62166d9a329c735fee8eca6
MachoMecha <fernandocavo@gmail.com>
2016-06-25 12:33:38 -0300

    changes.sql

--- documentos/changes.sql
+++ documentos/changes.sql
@@ -1,39 +1,39 @@
 
-ALTER TABLE MasterTk.Equipo DROP FOREIGN KEY Equipo_ibfk_1;
-ALTER TABLE MasterTk.Equipo
+ALTER TABLE Equipo DROP FOREIGN KEY Equipo_ibfk_1;
+ALTER TABLE Equipo
 ADD CONSTRAINT Equipo_ibfk_1
 FOREIGN KEY (idProyecto) REFERENCES Proyecto (idProyecto) ON DELETE CASCADE;
 
 
-ALTER TABLE MasterTk.TipoItem DROP FOREIGN KEY TipoItem_ibfk_1;
-ALTER TABLE MasterTk.TipoItem
+ALTER TABLE TipoItem DROP FOREIGN KEY TipoItem_ibfk_1;
+ALTER TABLE TipoItem
 ADD CONSTRAINT TipoItem_ibfk_1
 FOREIGN KEY (idProyecto) REFERENCES Proyecto (idProyecto) ON DELETE CASCADE;
 
 
-ALTER TABLE MasterTk.Estado DROP FOREIGN KEY Estado_ibfk_1;
-ALTER TABLE MasterTk.Estado
+ALTER TABLE Estado DROP FOREIGN KEY Estado_ibfk_1;
+ALTER TABLE Estado
 ADD CONSTRAINT Estado_ibfk_1
 FOREIGN KEY (idTipoItem) REFERENCES TipoItem (idTipoItem) ON DELETE CASCADE;
 
 
 
 
-ALTER TABLE MasterTk.EquipoAtencion DROP FOREIGN KEY EquipoAtencion_ibfk_1;
-ALTER TABLE MasterTk.EquipoAtencion
+ALTER TABLE EquipoAtencion DROP FOREIGN KEY EquipoAtencion_ibfk_1;
+ALTER TABLE EquipoAtencion
 ADD CONSTRAINT EquipoAtencion_ibfk_1
 FOREIGN KEY (idEquipo) REFERENCES Equipo (idEquipo) ON DELETE CASCADE;
 
 
 
-ALTER TABLE MasterTk.EquipoAtencion DROP FOREIGN KEY EquipoAtencion_ibfk_2;
-ALTER TABLE MasterTk.EquipoAtencion
+ALTER TABLE EquipoAtencion DROP FOREIGN KEY EquipoAtencion_ibfk_2;
+ALTER TABLE EquipoAtencion
 ADD CONSTRAINT EquipoAtencion_ibfk_2
 FOREIGN KEY (idEstado) REFERENCES Estado (idEstado) ON DELETE CASCADE;
 
 
-ALTER TABLE MasterTk.UsuarioRolEquipo DROP FOREIGN KEY UsuarioRolEquipo_ibfk_3;
-ALTER TABLE MasterTk.UsuarioRolEquipo
+ALTER TABLE UsuarioRolEquipo DROP FOREIGN KEY UsuarioRolEquipo_ibfk_3;
+ALTER TABLE UsuarioRolEquipo
 ADD CONSTRAINT UsuarioRolEquipo_ibfk_3
 FOREIGN KEY (idEquipo) REFERENCES Equipo (idEquipo) ON DELETE CASCADE;
 
